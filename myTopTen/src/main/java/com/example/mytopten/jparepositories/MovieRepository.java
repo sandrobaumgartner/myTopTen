308ea3f624db8bfe16a57d73201f962cda9c477a
sandrobaumgartner <s50678@edu.campus02.at>
2023-03-31 16:57:01 +0200

    implemented some basic sql statements in jpa for later usage

---
+++ myTopTen/src/main/java/com/example/mytopten/jparepositories/MovieRepository.java
@@ -0,0 +1,18 @@
+package com.example.mytopten.jparepositories;
+
+import com.example.mytopten.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    Optional<Movie> findById(Long id);
+
+    @Query("SELECT m FROM Movie m WHERE LOWER(m.title) LIKE LOWER(CONCAT('%', :partOfTitle,'%'))")
+    Optional<List<Movie>> getMoviesByPartOfTitle(String partOfTitle);
+}
